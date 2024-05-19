71c55753200b07925e9a74646c263cd45e8934f4
miracoly <68049792+miracoly@users.noreply.github.com>
2024-05-19 16:12:11 +0200

    feat(c): add kata 27. remove element

---
+++ 27-remove-element/c/remove-element.c
@@ -0,0 +1,12 @@
+#include <stddef.h>
+#include "remove-element.h"
+
+int removeElement(int* nums, int numsSize, int val) {
+    int k = 0;
+    for (size_t i = 0; (int) i < numsSize; ++i) {
+        if (nums[i] != val) {
+            nums[k++] = nums[i];
+        }
+    }
+    return k;
+}
---
+++ 27-remove-element/c/remove-element.h
@@ -0,0 +1,6 @@
+#ifndef LEETCODE_H
+#define LEETCODE_H
+
+int removeElement(int* nums, int numsSize, int val);
+
+#endif
