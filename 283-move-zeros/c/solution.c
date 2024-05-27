a2a99958bba20427650a62ff3a63ac9a9db21c1c
miracoly <68049792+miracoly@users.noreply.github.com>
2024-05-27 19:49:32 +0200

    feat(c): add 283. move zeros

---
+++ 283-move-zeros/c/solution.c
@@ -0,0 +1,17 @@
+#include <stddef.h>
+#include "solution.h"
+
+void moveZeroes(int* nums, int numsSize) {
+    if (!nums || numsSize < 1) return;
+
+    size_t k = 0;
+    for (size_t i = 0; (int) i < numsSize; ++i) {
+        if (nums[i]) {
+            nums[k++] = nums[i];
+        }
+    }
+
+    for (size_t i = k; (int) i < numsSize; ++i) {
+        nums[i] = 0;
+    }
+}
---
+++ 283-move-zeros/c/solution.h
@@ -0,0 +1,6 @@
+#ifndef LEETCODE_H
+#define LEETCODE_H
+
+void moveZeroes(int* nums, int numsSize);
+
+#endif
