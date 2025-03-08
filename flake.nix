8c7ab5b3123639d0762461b1029f61c0b70b0893
miracoly <68049792+miracoly@users.noreply.github.com>
2025-03-08 20:03:11 +0100

    build: add nix flake

---
+++ flake.nix
@@ -0,0 +1,17 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
+  };
+
+  outputs = {nixpkgs, ...}: let
+    system = "x86_64-linux";
+    pkgs = import nixpkgs {inherit system;};
+  in {
+    devShell.${system} = pkgs.mkShell {
+      buildInputs = with pkgs; [criterion gcc gnumake uthash];
+      shellHook = ''
+        bear -- make > /dev/null 2>&1
+      '';
+    };
+  };
+}
--- shell.nix
+++
@@ -1,8 +0,0 @@
-{ pkgs ? import <nixpkgs> {} }:
-pkgs.mkShell {
-    buildInputs = with pkgs.buildPackages; [
-        gnumake
-        gcc
-        uthash
-    ];
-}
